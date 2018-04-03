f7f6852198281d15e207054389d997a91403a7ca
Nabanarendra Dhall <narendra.dhall@techeassystemse.in>
2018-04-03 12:28:55 +0530

    Git ignore testing

--- src/main/java/com/HReckAutomation/CommonUtils/TakeScteenshot.java
+++ src/main/java/com/HReckAutomation/CommonUtils/TakeScteenshot.java
@@ -12,16 +12,17 @@ import org.openqa.selenium.TakesScreenshot;
 
 public class TakeScteenshot {
 	public static String folderName() {
-	DateFormat df = new SimpleDateFormat("dd_MM_yyyy");
-	Date dateobj = new Date();
-//	System.out.println(df.format(dateobj));
-	String folder;
-	return  folder="D:\\"+"Failed Screenshot "+df.format(dateobj)+"\\ ";
+		DateFormat df = new SimpleDateFormat("dd_MM_yyyy");
+		Date dateobj = new Date();
+		// System.out.println(df.format(dateobj));
+		String folder;
+		return folder = "D:\\" + "Failed Screenshot " + df.format(dateobj) + "\\ ";
 	}
-	public static void getscreenshot( String filename) throws IOException {
+
+	public static void getscreenshot(String filename) throws IOException {
 
 		try {
-			File source = ((TakesScreenshot)CommonUtilities.driver).getScreenshotAs(OutputType.FILE);
+			File source = ((TakesScreenshot) CommonUtilities.driver).getScreenshotAs(OutputType.FILE);
 			FileUtils.copyFile(source, new File(TakeScteenshot.folderName() + filename + ".png"));
 		} catch (Exception e) {
 			System.out.println("Failed to take screen shot due to: " + e.getMessage());
