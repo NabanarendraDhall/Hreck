35abd2ba04c52c27f19d9448b2af0db501488b9f
Nabanarendra Dhall <narendra.dhall@techeassystemse.in>
2018-09-18 15:20:41 +0530

    test class and excel updated.

---
+++ src/main/java/com/HReckAutomation/HReck/TestForVSTS.java
@@ -0,0 +1,14 @@
+package com.HReckAutomation.HReck;
+
+import org.testng.annotations.Test;
+
+import com.mentorstudies.automationframework.common.TestClassUtil;
+
+public class TestForVSTS extends TestClassUtil {
+	
+	@Test
+	public void test() {
+		System.out.println("Test case pass");
+	}
+
+}
